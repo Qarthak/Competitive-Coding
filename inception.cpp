efb17b9168e2e953f8d6064564b816bd3af618a3
Qarthak <81775691+Qarthak@users.noreply.github.com>
2021-10-31 23:38:44 +0530

    Create inception.cpp

---
+++ inception.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <algorithm>
+#include <deque>
+#include <iterator>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <utility>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	#include <iostream>
+	#include <algorithm>
+	#include <deque>
+	#include <iterator>
+	#include <list>
+	#include <map>
+	#include <queue>
+	#include <set>
+	#include <stack>
+	#include <string>
+	#include <utility>
+	
+	using namespace std;
+	
+	int main(int argc, char const *argv[])
+	{
+		#include <iostream>
+		#include <algorithm>
+		#include <deque>
+		#include <iterator>
+		#include <list>
+		#include <map>
+		#include <queue>
+		#include <set>
+		#include <stack>
+		#include <string>
+		#include <utility>
+		
+		using namespace std;
+		
+		int main(int argc, char const *argv[])
+		{
+			#include <iostream>
+			#include <algorithm>
+			#include <deque>
+			#include <iterator>
+			#include <list>
+			#include <map>
+			#include <queue>
+			#include <set>
+			#include <stack>
+			#include <string>
+			#include <utility>
+			
+			using namespace std;
+			
+			int main(int argc, char const *argv[])
+			{
+				
+			}
+		}
+	}
+}
